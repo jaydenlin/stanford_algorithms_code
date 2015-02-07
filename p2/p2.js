bb772f079c40c819cc297a63567c930e3b965c44
JaydenLin <j5905653@gmail.com>
2015-02-08 00:14:01 +0800

    quicksort scaffold

---
+++ p2/p2.js
@@ -0,0 +1,21 @@
+
+var arrayData=[];
+//always choose first element as pivot
+var partition=function(arrayData,p,r){
+  
+  
+  
+}
+
+
+var quicksort=function(arrayData,p,r){
+    if(p<r){
+      var q= partition(arrayData,p,r);
+      quicksort(arrayData,p,r);
+      quicksort(arrayData,p,r);
+    }
+
+};
+
+
+

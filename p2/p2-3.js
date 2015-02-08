2b8434896e507b54d8644309c736af29ba349e29
JaydenLin <j5905653@gmail.com>
2015-02-08 16:49:21 +0800

    finish median as pivot

--- p2/p2-3.js
+++ p2/p2-3.js
@@ -1,15 +1,41 @@
 var fs=require('fs');
 var totalOfComparisons=0;
 var arrayData=[];
-//always choose first element as pivot
+//always choose last element as pivot
 var partition=function(arrayData,p,r){
     var m=r-p+1; 
     var i=p;
     var j=p+1;
     totalOfComparisons = totalOfComparisons + m-1;
-    //choose first element as pivot
-    var x=arrayData[p];
-    
+    //choose middle element as pivot
+    var middleIndex= parseInt((p+r)/2,10);
+
+    console.log("pivot:??"+" form "+ arrayData[p]+" "+arrayData[r]+" "+arrayData[middleIndex]); 
+    var middleCadidate=[];
+    middleCadidate.push(parseInt(arrayData[p],10),parseInt(arrayData[r],10),parseInt(arrayData[middleIndex],10));
+    middleCadidate=middleCadidate.sort(function(a,b){
+       return a-b;
+    });    
+    if(middleCadidate[1]===arrayData[p]){
+        middleIndex=p; 
+    }
+
+    if(middleCadidate[1]===arrayData[middleIndex]){
+        middleIndex=middleIndex;    
+    }
+
+    if(middleCadidate[1]===arrayData[r]){
+        middleIndex=r;    
+    }
+    console.log("pivot:"+arrayData[middleIndex]); 
+
+    var x=arrayData[middleIndex];
+    //homework requirement, in real case this step is not really needed:
+    //before the main Partition subroutine, you should exchange the pivot element (i.e., the last element) with the first element
+    var tmp=arrayData[p];
+    arrayData[p]=arrayData[middleIndex];
+    arrayData[middleIndex]=tmp;
+
     while(j<=r){
       
       if(arrayData[j]>x){
@@ -56,3 +82,5 @@ for (var l in lines){
 var a=quicksort(testData,0,testData.length-1);
 console.log(a);
 console.log(totalOfComparisons);
+
+
